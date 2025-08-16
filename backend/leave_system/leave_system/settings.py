6663370c0d55da22de2fa37d2862b35c220c1e7e
dulkarsaman <dulkarsaman7@gmail.com>
2025-08-16 21:51:12 +0530

    implemented docker

--- backend/leave_system/leave_system/settings.py
+++ backend/leave_system/leave_system/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h3g@!3#9(wlu4%!%6)y=4ut8=73_+-ew(2%kck2$#zg^dtb!3x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -92,11 +93,11 @@ WSGI_APPLICATION = 'leave_system.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'boarding1db',
-        'USER': 'postgres',
-        'PASSWORD': 'dulkar123',
-        'HOST': 'localhost',  # or your DB host
-        'PORT': '5432',       # default PostgreSQL port
+        'NAME': config("DB_NAME"),
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_PASSWORD"),
+        'HOST': config("DB_HOST", default="db"), 
+        'PORT': config("DB_PORT", default="5432"),    
     }
 }
 
