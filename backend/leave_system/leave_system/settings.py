4edb3892985016f969fa5053ce589ccc803450f8
dulkarsaman <dulkarsaman7@gmail.com>
2025-08-16 22:09:16 +0530

    fix:added credentials to env file

--- backend/leave_system/leave_system/settings.py
+++ backend/leave_system/leave_system/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".onrender.com", 
+]
 
 
 # Application definition
